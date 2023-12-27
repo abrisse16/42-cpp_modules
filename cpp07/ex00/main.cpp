f980228a35b027eb6a1e1c71e80de39a217bf016
Alexis Brisse <abrisse@student.42.fr>
2023-12-27 13:54:29 +0100

    cpp07: add some tests from the evaluation

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: abrisse <abrisse@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/17 17:36:04 by abrisse           #+#    #+#             */
-/*   Updated: 2023/12/26 16:47:42 by abrisse          ###   ########.fr       */
+/*   Updated: 2023/12/27 13:26:47 by abrisse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,62 @@
 
 #include "whatever.hpp"
 
+class Awesome
+{
+	public:
+
+		Awesome(void) : _n(0) {}
+		Awesome(int n) : _n(n) {}
+		Awesome &	operator=(Awesome & a) {_n =a._n; return *this;}
+		bool operator==(Awesome const & rhs) const {return (this->_n == rhs._n);}
+		bool operator!=(Awesome const & rhs) const {return (this->_n != rhs._n);}
+		bool operator>(Awesome const & rhs) const {return (this->_n > rhs._n);}
+		bool operator<(Awesome const & rhs) const {return (this->_n < rhs._n);}
+		bool operator>=(Awesome const & rhs) const {return (this->_n >= rhs._n);}
+		bool operator<=(Awesome const & rhs) const {return (this->_n <= rhs._n);}
+		int	get_n() const { return _n; }
+
+	private:
+	
+		int	_n;
+};
+
+std::ostream & operator<<(std::ostream & o, const Awesome &a) { o << a.get_n(); return o; }
+
 int	main( void )
 {
-	int	a = 2;
-	int	b = 3;
+	{
+		std::cout << "-------------------------------------------" << std::endl;
 
-	std::cout << "a = " << a << ", b = " << b << std::endl;
-	::swap( a, b );
-	std::cout << "a = " << a << ", b = " << b << std::endl;
-	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+		int	a = 2;
+		int	b = 3;
 
-	std::string	c = "chaine1";
-	std::string	d = "chaine2";
+		std::cout << "a = " << a << ", b = " << b << std::endl;
+		::swap( a, b );
+		std::cout << "a = " << a << ", b = " << b << std::endl;
+		std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+		std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
 
-	std::cout << "c = " << c << ", d = " << d << std::endl;
-	::swap(c, d);
-	std::cout << "c = " << c << ", d = " << d << std::endl;
-	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+		std::string	c = "chaine1";
+		std::string	d = "chaine2";
+
+		std::cout << "c = " << c << ", d = " << d << std::endl;
+		::swap(c, d);
+		std::cout << "c = " << c << ", d = " << d << std::endl;
+		std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+		std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	}
+
+	{
+		std::cout << "-------------------------------------------" << std::endl;
+		
+		Awesome	a(2), b(4);
+
+		swap(a, b);
+		std::cout << a << " " << b << std::endl;
+		std::cout << max(a,b) << std::endl;
+		std::cout << min(a,b) << std::endl;
+	}
 
 	return 0;
 }
--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -6,12 +6,30 @@
 /*   By: abrisse <abrisse@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/26 16:57:05 by abrisse           #+#    #+#             */
-/*   Updated: 2023/12/26 22:51:54 by abrisse          ###   ########.fr       */
+/*   Updated: 2023/12/27 13:41:07 by abrisse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "iter.hpp"
+
+class Awesome
+{
+	public:
+	
+		Awesome(void): _n(42) {return;}
+		int	get(void) const {return this->_n;}
+
+	private:
+	
+		int _n;
+};
+
+std::ostream &	operator<<(std::ostream & o, Awesome const & rhs) { o << rhs.get(); return o; }
+
+template<typename T>
+void print(T & x) {std::cout << x << std::endl; return;}
+
 
 void	square(int &i)
 {
@@ -20,17 +38,33 @@ void	square(int &i)
 
 int	main( void )
 {
-	int			intArray[5]		= {1, 2, 3, 4, 5};
-	std::string	stringArray[2]	= {"42", "quarante-deux"};
+	{
+		std::cout << "-------------------------------------------" << std::endl;
 
-	::iter(intArray, 5, ::printData);
+		int			intArray[5]		= {1, 2, 3, 4, 5};
+		std::string	stringArray[2]	= {"42", "quarante-deux"};
 
-	::iter(intArray, 5, square);
-	std::cout << std::endl << "square function..." << std::endl;
+		::iter(intArray, 5, ::printData);
 
-	::iter(intArray, 5, ::printData);
+		::iter(intArray, 5, square);
+		std::cout << std::endl << "square function..." << std::endl;
 
-	std::cout << std::endl;
-	::iter(stringArray, 2, ::printData);
+		::iter(intArray, 5, ::printData);
 
+		std::cout << std::endl;
+		::iter(stringArray, 2, ::printData);
+	}
+
+	{
+		std::cout << "-------------------------------------------" << std::endl;
+
+		int	tab[] = {0,1,2,3,4};
+		
+		Awesome tab2[5];
+
+		iter(tab, 5, print);
+		iter(tab2, 5, print);
+	}
+
+	return 0;
 }
